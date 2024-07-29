516ba601e4e431a8a52e47f3914b749f7aa39e86
Lawrence Babb <lbabb@broadinstitute.org>
2024-07-29 18:01:47 -0400

    fixup-depoyment-action

--- scripts/service-account-permission-setup.sh
+++ scripts/service-account-permission-setup.sh
@@ -7,6 +7,9 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
   --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
   --role="roles/storage.objectAdmin" \
   --role="roles/bigquery.dataEditor" \
-  --role="roles/bigquery.jobUser"
+  --role="roles/bigquery.jobUser" \
+  --role="roles/bigquery.user" \
+  --condition=None \
+  --format=json
 
 echo "Service account permissions setup completed."
