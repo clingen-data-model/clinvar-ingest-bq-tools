939ae2b21e41d6a4224e499da58abc7d3abd3fbc
Lawrence Babb <lbabb@broadinstitute.org>
2024-09-30 21:58:26 -0400

    fixed func name

---
+++ scripts/general/bq-createSigType-func.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION `clinvar_ingest.createSigType`(nosig_count INTEGER, unc_count INTEGER, sig_count INTEGER)
+RETURNS ARRAY<STRUCT<count INTEGER, percent NUMERIC>>
+LANGUAGE js  
+  OPTIONS (
+    library=['gs://clinvar-ingest/bq-tools/bq-utils.js'])
+AS r"""
+  return createSigType(nosig_count, unc_count, sig_count);
+""";
