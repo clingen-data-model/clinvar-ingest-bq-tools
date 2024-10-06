7a03524f93bf50632764cad0c00d5e854af40a9d
Lawrence Babb <lbabb@broadinstitute.org>
2024-10-05 21:33:15 -0400

    almost there.

--- scripts/temporal-table-procs/11-voi-and-voi-scv-group-proc.sql
+++ scripts/temporal-table-procs/11-voi-and-voi-scv-group-proc.sql
@@ -102,6 +102,7 @@ BEGIN
 
 END;
 
+
 -- find intersection between voi and voi_scv windows for the same variant to create the voi_group records
 -- date window intersection is found by using the condition ((start_window1 <= end_window2) AND (end_window1 >= start_window2))
 -- the start and end dates are always inclusive, meaning the start date is the date that the record is first available and
